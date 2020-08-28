5579dfbbdf1561f10ee6174283cf986b25b4edb3
Colin Hale <colinah@colins-mbp.lan>
2020-08-27 23:30:01 -0600

    Finished problem

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,10 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+    let iseven: number = 0;
+    if(a.length%2 === 0) iseven = 1;
+    return a[Math.floor(a.length/2) - iseven];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
